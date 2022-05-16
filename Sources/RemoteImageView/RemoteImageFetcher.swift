5bc6a80b7dfcf079c6545a59aa3ec8545d2d208f
Khoa Pham <khoa.pham@tyme.com>
2022-05-16 20:29:34 +0700

    Update init method

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -32,9 +32,9 @@ public class RemoteImageFetcher: ObservableObject {
   @Published var imageData = Data()
   let url: URL
 
-  public init(url: URL) {
-    self.url = url
-  }
+    public init(from url: URL) {
+      self.url = url
+    }
 
   // 1
   public func fetch() {
